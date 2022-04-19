3cc9762b8050a0b61f38076e94b0339b37a849d3
Ghack81 <herve-mention@hotmail.com>
2022-04-19 11:50:11 +0200

    Some Type Variables

---
+++ variables.cs
@@ -0,0 +1,16 @@
+// C# Variables
+using system;
+
+namespace MyApplication
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string name = "John";
+        Console.WriteLine(name);
+
+        Console.ReadLine();
+    }
+  }
+}
