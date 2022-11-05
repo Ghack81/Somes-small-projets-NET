b90c06ab9497c6ce49a582a4f88a09a1f8165888
Ghack81 <herve-mention@hotmail.com>
2022-11-05 20:39:27 +0100

    Examples Variable

---
+++ Variables/Varibles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Varibles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nombre;
+            int nombre2 = 123;
+            Boolean PartieFinie = false;
+            string chaine = "azerty123456";
+
+            Console.WriteLine(nombre2);
+            Console.WriteLine(PartieFinie);
+            Console.ReadKey();
+        }
+    }
+}
